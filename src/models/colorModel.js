1714f54cf2190c189cca9c58173652ee3ba40db9
Anurag Kashyap <anuragkashyap815353@gmail.com>
2024-03-18 20:01:44 +0530

    color

--- src/models/colorModel.js
+++ src/models/colorModel.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 const colorSchema = mongoose.Schema({
-    _id: {
-        type: String, 
-        required: true
-    },
     color: {
         required: true,
         type: String,
